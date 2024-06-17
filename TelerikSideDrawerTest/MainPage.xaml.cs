54de5e3ddf3ceec1f3c55f45b05f666f14d628ec
Eduardo Reis <eduardoreisdev@gmail.com>
2024-06-17 10:22:00 -0300

    Initial commit

---
+++ TelerikSideDrawerTest/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace TelerikSideDrawerTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
